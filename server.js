3b0b49f62c5d312ea7eabd825e2b1dc2ccd61d06
boddu shivasai <shivasai@mightyapps.ai>
2025-03-13 16:52:53 +0530

    Add/Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
- 
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const ap = express();
+const app = express();
 
 // Parse JSON bodies
 app.use(bodyParser.json());
